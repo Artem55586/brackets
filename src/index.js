850edac0fabe3f76d0a38eb17bbdd6f5046f0879
Artem55586 <artem555_86@ukr.net>
2023-01-19 02:08:08 +0100

    feat:done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let newString = '';
+  let concurrence = false;
+  
+  transformation: for (let i = 0; i < str.length; i++) {
+    for (let n = 0; n < bracketsConfig.length; n++) {
+      if (str[i] === bracketsConfig[n][0] && str[i + 1] === bracketsConfig[n][1]) {
+        newString = str.slice(0, i) + str.slice(i + 2);      
+        concurrence = true;
+        break transformation;
+      } else {
+        newString = str;
+      }
+    }
+  }    
+
+  if (concurrence === true && newString.length > 0) {
+    return check(newString, bracketsConfig);
+  } else {
+    if (newString.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }  
 }
